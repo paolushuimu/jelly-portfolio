3cd762bb52d61166343e8a84b6e37fb6aa66afd1
Jelly Alyssa <211906758@qq.com>
2025-07-12 13:44:18 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 
 import Footer from "./components/Footer";
 import Project from "./pages/Project";
-import LogoPage from "./pages/Logo";
+// import LogoPage from "./pages/Logo";
 
 function App() {
   return (
     <div className="w-[80rem] min-h-screen text-gray-800 font-sans">
       <div className="w-[80rem] mx-auto">
         <Routes>
-          <Route path="/" element={<LogoPage />}></Route>
+          {/* <Route path="/" element={<LogoPage />}></Route> */}
           <Route path="/projects" element={<Project />}></Route>
         </Routes>
       </div>
